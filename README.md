59bbe3fc67fa476109d07f1711c3537f648a2e1b
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-06-14 15:21:36 -0500

    add objects with the following properties: state, city, landmarks, year

---
+++ README.md
@@ -0,0 +1,15 @@
+Destinations ()
+
+Test: "It will create an object for each destination that will have the following properties: state, city, landmarks, year"
+Code: 
+let destination1 = {
+  state: "FL",
+  city: "Miami",
+  landmarks: ["Hollywood Beach", "Havana street"], 
+  year: 2021
+};
+Expected Output: 
+  state: "FL",
+  city: "Miami",
+  landmarks: ["Hollywood Beach", "Havana street"], 
+  year: 2021
---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+
+
+
+//Objects with properties 
+let destination1 = {
+  state: "FL",
+  city: "Miami",
+  landmarks: ["Hollywood Beach", "Havana street"], 
+  year: 2021
+}
+
+let destination2 = {
+  state: "FL",
+  city: "Orlando",
+  landmarks: ["Disney World", "Universal Studios"], 
+  year: 2021
+}
+
+let destination3 = {
+  state: "WY",
+  city: "Jackson hole",
+  landmarks: ["Grand Teton National Park", "Yellowstone National Park"], 
+  year: 2020
+}
+
+let destination4 = {
+  state: "MA",
+  city: "Boston",
+  landmarks: ["Harvard University", "Boston Marketplace"], 
+  year: 2019
+}
+
+let destination5 = {
+  state: "CA",
+  city: "Vancouver",
+  landmarks: ["Capilano Suspension Bridge Park", "Cypress Mountain ski resort"], 
+  year: 2019
+}
